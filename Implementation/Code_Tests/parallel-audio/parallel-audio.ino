308b6b70dd568776586e1a09537134fd22118f96
Johannes Waltmann <johannes.waltmann@stud.uni-due.de>
2021-03-26 11:48:01 +0100

    Bugfixes on Server with Concurrent Recording and Replay

--- Implementation/Code_Tests/parallel-audio/parallel-audio.ino
+++ Implementation/Code_Tests/parallel-audio/parallel-audio.ino
@@ -1,6 +1,7 @@
 // https://github.com/fabiuz7/melody-player-arduino
 #include "melody_player.h"
 #include "melody_factory.h"
+#include "requirements.h"
 
 #include <SD.h>
 #include "heltec.h"
@@ -9,6 +10,10 @@
 
 #include <WiFi.h>
 #include "time.h"
+#include <SPI.h>
+#include <FS.h>
+
+#include "stdio.h"
 
 // Params for the WiFi Connection
 WiFiServer server(80); // Opens a Server on Port 80
@@ -99,7 +104,7 @@ void setup() {
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay();
+    delay(500);
     Serial.print(".");
   }
 
@@ -139,7 +144,7 @@ void loop() {
         if (c == '\n') {
           if (currentLine.length()  == 0) {
             client.println("HTTP/1.1 200 OK");
-            client.println("Contnet-type:text/html");
+            client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
 
@@ -227,6 +232,7 @@ void loop() {
     // TODO Wenn run_melody funktioniert
 //    if (run_high) {}
 //    if (run_low) {}
+}
 }
 
 // TODO Comments
@@ -356,54 +362,54 @@ void i2s_adc(void) {
   flash_write_buff = NULL;
 }
 
-// TODO Comments
-void wavHeader(byte* header, int wavSize) {
-  header[0] = 'R';
-  header[1] = 'I';
-  header[2] = 'F';
-  header[3] = 'F';
-  unsigned int fileSize =  wavSize + headerSize - 8;
-  header[4] = (byte)(fileSize & 0xFF);
-  header[5] = (byte)((fileSize >> 8) & 0xFF);
-  header[6] = (byte)((fileSize >> 16) & 0xFF);
-  header[7] = (byte)((fileSize >> 24) & 0xFF);
-  header[8] = 'W';
-  header[9] = 'A';
-  header[10] = 'V';
-  header[11] = 'E';
-  header[12] = 'f';
-  header[13] = 'm';
-  header[14] = 't';
-  header[15] = ' ';
-  header[16] = 0x10;  // linear PCM
-  header[17] = 0x00;
-  header[18] = 0x00;
-  header[19] = 0x00;
-  header[20] = 0x01;  // linear PCM
-  header[21] = 0x00;
-  header[22] = 0x01;  // monoral
-  header[23] = 0x00;
-  header[24] = 0x80;  // sampling rate 44100
-  header[25] = 0x3E;
-  header[26] = 0x00;
-  header[27] = 0x00;
-  header[28] = 0x00;  // Byte/sec = 44100x2x1 = 88200
-  header[29] = 0x7D;
-  header[30] = 0x01;
-  header[31] = 0x00;
-  header[32] = 0x02;  // 16bit monoral
-  header[33] = 0x00;
-  header[34] = 0x10;  // 16bit
-  header[35] = 0x00;
-  header[36] = 'd';
-  header[37] = 'a';
-  header[38] = 't';
-  header[39] = 'a';
-  header[40] = (byte)(wavSize & 0xFF);
-  header[41] = (byte)((wavSize >> 8) & 0xFF);
-  header[42] = (byte)((wavSize >> 16) & 0xFF);
-  header[43] = (byte)((wavSize >> 24) & 0xFF);
-}
+//// TODO Comments
+//void wavHeader(byte* header, int wavSize) {
+//  header[0] = 'R';
+//  header[1] = 'I';
+//  header[2] = 'F';
+//  header[3] = 'F';
+//  unsigned int fileSize =  wavSize + headerSize - 8;
+//  header[4] = (byte)(fileSize & 0xFF);
+//  header[5] = (byte)((fileSize >> 8) & 0xFF);
+//  header[6] = (byte)((fileSize >> 16) & 0xFF);
+//  header[7] = (byte)((fileSize >> 24) & 0xFF);
+//  header[8] = 'W';
+//  header[9] = 'A';
+//  header[10] = 'V';
+//  header[11] = 'E';
+//  header[12] = 'f';
+//  header[13] = 'm';
+//  header[14] = 't';
+//  header[15] = ' ';
+//  header[16] = 0x10;  // linear PCM
+//  header[17] = 0x00;
+//  header[18] = 0x00;
+//  header[19] = 0x00;
+//  header[20] = 0x01;  // linear PCM
+//  header[21] = 0x00;
+//  header[22] = 0x01;  // monoral
+//  header[23] = 0x00;
+//  header[24] = 0x80;  // sampling rate 44100
+//  header[25] = 0x3E;
+//  header[26] = 0x00;
+//  header[27] = 0x00;
+//  header[28] = 0x00;  // Byte/sec = 44100x2x1 = 88200
+//  header[29] = 0x7D;
+//  header[30] = 0x01;
+//  header[31] = 0x00;
+//  header[32] = 0x02;  // 16bit monoral
+//  header[33] = 0x00;
+//  header[34] = 0x10;  // 16bit
+//  header[35] = 0x00;
+//  header[36] = 'd';
+//  header[37] = 'a';
+//  header[38] = 't';
+//  header[39] = 'a';
+//  header[40] = (byte)(wavSize & 0xFF);
+//  header[41] = (byte)((wavSize >> 8) & 0xFF);
+//  header[42] = (byte)((wavSize >> 16) & 0xFF);
+//  header[43] = (byte)((wavSize >> 24) & 0xFF);
+//}
 
 /**
    Scale up the recorded sound data to increase its volume
